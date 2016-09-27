75dfeb61411194743c79e030045b145eeea43411
Junpeng Qiu <qjpchmail@gmail.com>
2016-09-27 17:12:05 -0400

    Correct indentation

--- gscholar-bibtex.el
+++ gscholar-bibtex.el
@@ -608,24 +608,24 @@
 ;;; Google Scholar
 (defun gscholar-bibtex-google-scholar-search-results (query)
   (let* ((url-request-method "GET")
-	 ;; Fabricate a cookie with a random ID that expires in an hour.
+         ;; Fabricate a cookie with a random ID that expires in an hour.
          (random-id (format "%016x" (random (expt 16 16))))
-	 (expiration (format-time-string "%a, %d %b %Y %H:%M:%S.00 %Z"
-					 (time-add (current-time)
-						   (seconds-to-time 3600)) t))
+         (expiration (format-time-string "%a, %d %b %Y %H:%M:%S.00 %Z"
+                                         (time-add (current-time)
+                                                   (seconds-to-time 3600)) t))
          (my-cookie (mapconcat #'identity
-			       (list (format "GSP=ID=%s:CF=4; " random-id)
-				     (format "expires=%s" expiration)
-				     "path=/"
-				     "domain=scholar.google.com")
-			       "; ")))
+                               (list (format "GSP=ID=%s:CF=4; " random-id)
+                                     (format "expires=%s" expiration)
+                                     "path=/"
+                                     "domain=scholar.google.com")
+                               "; ")))
     (let ((url-current-object
-	   (url-generic-parse-url "http://scholar.google.com") ))
+           (url-generic-parse-url "http://scholar.google.com") ))
       (url-cookie-handle-set-cookie my-cookie))
     (gscholar-bibtex--url-retrieve-as-string
      (concat "http://scholar.google.com/scholar?q="
-	     (url-hexify-string
-	      (replace-regexp-in-string " " "\+" query))))))
+             (url-hexify-string
+              (replace-regexp-in-string " " "\+" query))))))
 
 (defun gscholar-bibtex-google-scholar-bibtex-urls (buffer-content)
   (gscholar-bibtex-re-search buffer-content "\\(/scholar\.bib.*?\\)\"" 1))
